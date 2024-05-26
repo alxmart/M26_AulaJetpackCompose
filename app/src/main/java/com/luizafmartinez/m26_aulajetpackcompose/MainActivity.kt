34f0b4e1ed1ea388f595fe62cbc64e8daa439ffd
alxmart <alxmartinez@gmail.com>
2024-05-26 16:01:29 -0300

    Testing modifiers: width, height, background, border, padding

--- app/src/main/java/com/luizafmartinez/m26_aulajetpackcompose/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m26_aulajetpackcompose/MainActivity.kt
@@ -7,21 +7,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.luizafmartinez.m26_aulajetpackcompose.componentes.Botao
 import com.luizafmartinez.m26_aulajetpackcompose.ui.theme.M26_AulaJetpackComposeTheme
@@ -63,8 +69,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PrimeiroApp() {
-    //Row
+
     Column(
+        modifier = Modifier
+            /* .width(200.dp)
+            .height(200.dp)*/
+            .background(Color.Gray)
+            .border(2.dp, Color.Red)
+            //.padding(30.dp, 10.dp)
+            .padding(30.dp)
+            //.padding(top = 30.dp, end = 30.dp)
+            /*.border(2.dp, Color.Green)
+            .padding(30.dp, 10.dp)
+            .border(2.dp, Color.Magenta)*/
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Text(text = "Joseph Nut", fontSize = 20.sp)
+
+        Column(modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+            .clip(CircleShape)
+            .background(Color.Green)
+            /*.padding(start = 8.dp,
+                top = 10.dp,
+                end = 30.dp,
+                bottom = 30.dp)*/
+        ) {
+            Text(text = "JN", fontSize = 32.sp)
+        }
+    }
+
+    //Row
+    /*Column(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
@@ -80,7 +118,7 @@ fun PrimeiroApp() {
        Botao(texto = "Jamilton") {
             println("executou")
         }
-    }
+    }*/
 
     /*Text(
         text = "John Doe",
